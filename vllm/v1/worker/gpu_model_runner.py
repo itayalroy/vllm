60de06eedda9df7cf4e50b840e9750675d9a31d0
Itay Alroy <ialroy@nvidia.com>
2026-07-16 17:35:26 +0300

    Keep GC heap frozen during graph recapture
    Signed-off-by: Itay Alroy <ialroy@nvidia.com>

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -6532,6 +6532,10 @@ class GPUModelRunner(
     @staticmethod
     @contextmanager
     def _freeze_gc():
+        if gc.get_freeze_count() > 0:
+            yield
+            return
+
         gc.collect()
         should_freeze = not envs.VLLM_ENABLE_CUDAGRAPH_GC
         if should_freeze:
